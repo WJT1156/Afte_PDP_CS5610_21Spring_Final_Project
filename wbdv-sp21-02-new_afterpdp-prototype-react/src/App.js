49ef882cf14bd9cae3fe475ec4165fa57c86b31d
liu-matt <matt_liu@apple.com>
2021-03-28 14:24:04 -0400

    movie search

---
+++ wbdv-sp21-02-new_afterpdp-prototype-react/src/App.js
@@ -0,0 +1,25 @@
+import SearchScreen from "./components/search-screen";
+import DetailsScreen from "./components/details-screen";
+import {BrowserRouter, Route} from "react-router-dom";
+import HomeScreen from "./components/home-screen";
+
+function App() {
+  return (
+      <div className="container-fluid">
+        <BrowserRouter>
+          <Route path="/" exact={true}>
+            <HomeScreen/>
+          </Route>
+          <Route path={["/search", "/search/:title"]}
+                 exact={true}>
+            <SearchScreen/>
+          </Route>
+          <Route path="/details/:imdbID" exact={true}>
+            <DetailsScreen/>
+          </Route>
+        </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
---
+++ wbdv-sp21-02-new_afterpdp-prototype-react/src/components/details-screen.js
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react'
+import {useHistory, useParams} from 'react-router-dom'
+import movieService from '../services/movie-service'
+
+const DetailsScreen = () => {
+    const {imdbID} = useParams()
+    const history = useHistory()
+    const [movie, setMovie] = useState({})
+    useEffect(() => {
+        findMovieByImdbID()
+    }, [])
+    const findMovieByImdbID = () => {
+        movieService.findMovieByImdbID(imdbID)
+            .then((data) => {
+                setMovie(data)
+            })
+    }
+    return(
+        <div>
+            <button onClick={()=>{history.goBack()}}>Back</button>
+            <h2>{movie.Title}</h2>
+            <p>
+                <img src={movie.Poster} width={100} style={{float: "right"}}/>
+                {movie.Plot}
+            </p>
+            <div>
+                {movie.Actors}
+                <ul>
+                    {
+                        movie.Actors && movie.Actors.split(",")
+                            .map((actor)=>{
+                                return(
+                                    <li>{actor}</li>
+                                )
+                            })
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default DetailsScreen
